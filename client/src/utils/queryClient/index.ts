0c65e77745a81984e77fc2191bcd3346a58fe489
yamashita.hiroyuki <hiroyuki-yamashita@sec-service.co.jp>
2026-04-10 01:52:00 +0900

    Moving queryClient and 401 setQueryData Client

---
+++ client/src/utils/queryClient/index.ts
@@ -0,0 +1,13 @@
+import { QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // API の性質に合わせて調整
+      staleTime: 1000 * 30, // 30秒は新鮮扱い
+      gcTime: 1000 * 60 * 10, // 10分でメモリから破棄
+      retry: 1, // 失敗時の自動リトライ回数
+      refetchOnWindowFocus: false, // フォーカスで再取得を抑止（必要なら true）
+    },
+  },
+});
