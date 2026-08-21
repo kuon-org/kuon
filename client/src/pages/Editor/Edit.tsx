f74003720eda423d05f824ae7d17e97212e1c656
yamashita.hiroyuki <hiroyuki-yamashita@sec-service.co.jp>
2026-08-22 08:00:26 +0900

    [modify] 403エラーの時通知&ルートにナビゲート

--- client/src/pages/Editor/Edit.tsx
+++ client/src/pages/Editor/Edit.tsx
@@ -1,14 +1,22 @@
+import { useNavigate } from "@tanstack/react-router";
 import Loading from "../../components/common/Loading/Loading";
 import ArticleEditor from "../../components/Editor";
 import { useArticles } from "../../hooks/useArticles";
+import { useNotify } from "../../hooks/useNotify";
 import { articleEditRoute } from "../../routes";
 
 export const Edit = () => {
   const { articleId } = articleEditRoute.useParams();
   const { article, isLoading, isOwned, editArticle, isEditing } =
     useArticles(articleId);
+  const { error } = useNotify();
+  const navigate = useNavigate();
   if (isLoading) return <Loading />;
-  if (!isOwned) return <>編集権限がありません</>;
+  if (!isOwned) {
+    error("編集権限がありません");
+    navigate({ to: "/" });
+  }
+
   return (
     <ArticleEditor
       article={article}
