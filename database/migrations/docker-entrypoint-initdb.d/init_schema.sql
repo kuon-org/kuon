93febc055c4646831d72268b290dc69ede34f228
hiroppe0409 <hiroppe0409@icloud.com>
2026-08-23 05:09:00 +0900

    Update init_schema.sql
    #17 で対応するため一時削除

--- database/migrations/docker-entrypoint-initdb.d/init_schema.sql
+++ database/migrations/docker-entrypoint-initdb.d/init_schema.sql
@@ -8,19 +8,6 @@ create table knowledge.kuon_migrations (
     primary key (name)
 );
 
-
-CREATE TABLE IF NOT EXISTS server_settings (
-    key VARCHAR(100) PRIMARY KEY,
-    value TEXT NOT NULL,
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-
-COMMENT ON TABLE knowledge.server_settings IS 'Kuonインスタンス全体のサーバ設定を管理するテーブル';
-
-COMMENT ON COLUMN knowledge.server_settings.key IS 'サーバ設定の識別子';
-COMMENT ON COLUMN knowledge.server_settings.value IS 'サーバ設定の値';
-COMMENT ON COLUMN knowledge.server_settings.updated_at IS 'サーバ設定が最後に更新された日時';
-
 -- users テーブル
 CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY DEFAULT uuidv7(),
