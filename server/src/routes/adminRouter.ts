e282be0df629b4a4b1fae6f37edefdc0b92afd86
hiroppe0409 <hiroppe0409@icloud.com>
2026-08-25 17:47:07 +0900

    feat: expose admin webhook routes

--- server/src/routes/adminRouter.ts
+++ server/src/routes/adminRouter.ts
@@ -6,11 +6,13 @@ import { AdminService } from "../services/adminService.js";
 import { AdminController } from "../controllers/adminController.js";
 import { BackupController } from "../controllers/backupController.js";
 import { RestoreController } from "../controllers/restoreController.js";
+import { WebhookController } from "../controllers/webhookController.js";
 import { authenticateToken } from "../middlewares/auth.js";
 import { AdminRepository } from "../repositories/adminRepository.js";
 import { serverSettingsService } from "../services/serverSettingsService.js";
 import { backupService } from "../services/backupService.js";
 import { restoreService } from "../services/restoreService.js";
+import { webhookService } from "../services/webhookService.js";
 
 const adminRouter = Router();
 const restoreUpload = multer({
@@ -24,11 +26,16 @@ const adminService = new AdminService(usersRepo, adminRepo);
 const adminController = new AdminController(adminService, serverSettingsService);
 const backupController = new BackupController(adminService, backupService);
 const restoreController = new RestoreController(adminService, restoreService);
+const webhookController = new WebhookController(adminService, webhookService);
 
 adminRouter.get("/admin/settings/users", authenticateToken, adminController.getUserList);
 adminRouter.post("/admin/settings/users/toggle_active/:userId", authenticateToken, adminController.toggleUserActive);
 adminRouter.get("/admin/settings/server", authenticateToken, adminController.getServerSettings);
 adminRouter.put("/admin/settings/server", authenticateToken, adminController.updateServerSetting);
+adminRouter.get("/admin/webhooks", authenticateToken, webhookController.getAll);
+adminRouter.get("/admin/webhooks/:id", authenticateToken, webhookController.getById);
+adminRouter.post("/admin/webhooks", authenticateToken, webhookController.create);
+adminRouter.delete("/admin/webhooks/:id", authenticateToken, webhookController.delete);
 adminRouter.post("/admin/backup/export", authenticateToken, backupController.exportBackup);
 adminRouter.post(
   "/admin/backup/restore",
