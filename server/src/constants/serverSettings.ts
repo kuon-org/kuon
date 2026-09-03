5c547ae4f9adb39a46ef07437ee902133fc64d14
hiroppe0409 <hiroppe0409@icloud.com>
2026-09-04 01:30:58 +0900

    feat: allow administrators to disable local account registration (#33) (#69)
    * feat: add local registration server setting
    
    * feat: support local registration setting
    
    * feat: add local registration availability service
    
    * feat: enforce local registration setting
    
    * feat: add local registration status hook
    
    * feat: hide registration link when disabled
    
    * feat: block registration page when disabled
    
    * feat: add local registration toggle
    
    * fix: refresh registration status after server setting update
    
    * docs: document local account registration setting
    
    * noop
    
    * chore: remove temporary file
    
    * docs: add issue 33 verification notes
    
    * docs: summarize issue 33 implementation
    
    * chore: remove issue-specific test notes
    
    * chore: remove redundant implementation note

--- server/src/constants/serverSettings.ts
+++ server/src/constants/serverSettings.ts
@@ -1,5 +1,6 @@
 export const ServerSettingKey = {
   AllowApiKey: "allow_api_key",
+  AllowLocalAccountRegistration: "allow_local_account_registration",
   RequireTotpForExternalIdp: "require_totp_for_external_idp",
   RequireAuthentication: "require_authentication",
   MaintenanceMode: "maintenance_mode",
