1e2fa6b65a8458c49568270e5fa3167eec6a837f
hiroppe0409 <hiroppe0409@icloud.com>
2026-09-10 02:13:18 +0900

    chore: export storage listing type

--- server/src/storage/index.ts
+++ server/src/storage/index.ts
@@ -3,6 +3,7 @@ export type {
   FileStorage,
   PutFileInput,
   StoredFile,
+  StoredFileInfo,
 } from "./FileStorage.js";
 export { LocalFileStorage } from "./LocalFileStorage.js";
 export { getFileStorage } from "./storageFactory.js";
