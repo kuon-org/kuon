8190809236f948010ebfa0247d41fd67925377a5
hiroppe0409 <hiroppe0409@icloud.com>
2026-09-10 01:48:12 +0900

    feat: export storage abstraction

---
+++ server/src/storage/index.ts
@@ -0,0 +1,9 @@
+export type {
+  FileBody,
+  FileStorage,
+  PutFileInput,
+  StoredFile,
+} from "./FileStorage.js";
+export { LocalFileStorage } from "./LocalFileStorage.js";
+export { getFileStorage } from "./storageFactory.js";
+export { storageConfig } from "./storageConfig.js";
