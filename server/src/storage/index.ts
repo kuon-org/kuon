05edc5fd58f37d9867fb9c04309bc0b737af17d7
hiroppe0409 <hiroppe0409@icloud.com>
2026-09-10 19:23:06 +0900

    chore: export S3-compatible storage provider

--- server/src/storage/index.ts
+++ server/src/storage/index.ts
@@ -6,5 +6,9 @@ export type {
   StoredFileInfo,
 } from "./FileStorage.js";
 export { LocalFileStorage } from "./LocalFileStorage.js";
+export {
+  S3CompatibleFileStorage,
+  type S3CompatibleStorageOptions,
+} from "./S3CompatibleFileStorage.js";
 export { getFileStorage } from "./storageFactory.js";
 export { storageConfig } from "./storageConfig.js";
